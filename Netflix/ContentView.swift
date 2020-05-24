c0ee1d8b9c0f1daaba05ddc55fb134833c14dce8
davalcato <davalcato@yahoo.com>
2020-05-24 16:01:17 -0700

    added the ScrollView(.horizontal, showsIndicators: false) to the struct bottomView : View inside the ContentView

--- Netflix/ContentView.swift
+++ Netflix/ContentView.swift
@@ -66,36 +66,56 @@ struct Home : View {
             
             ScrollView(.vertical, showsIndicators: false) {
                 
-                Text("Netflix Prime").font(.title)
-                
-                ZStack{
+                VStack(alignment: .leading, spacing: 15) {
                     
-                    Image("top").resizable()
+                    Text("Netflix Prime").font(.title)
                     
-                    VStack{
+                    ZStack{
                         
-                        Spacer()
-                        HStack{
-                            
-                            VStack(alignment: .leading, spacing: 10){
-                                
-                                Text("Marvel's The Defenders").font(.body)
-                                Text("Season 1").font(.caption)
-                            }
+                        Image("top").resizable()
+                        
+                        VStack{
                             
                             Spacer()
-                            
-                            Button(action: {
+                            HStack{
                                 
-                            }) {
+                                VStack(alignment: .leading, spacing: 10){
+                                    
+                                    Text("Marvel's The Defenders").font(.body)
+                                    Text("Season 1").font(.caption)
+                                }
                                 
-                                Image("play").renderingMode(.original)
+                                Spacer()
                                 
+                                Button(action: {
+                                    
+                                }) {
+                                    
+                                    Image("play").renderingMode(.original)
+                                    
+                                }
                             }
-                        }
-                    }.padding()
+                        }.padding()
+                        
+                    }.frame(height: 190)
                     
-                }.frame(height: 190)
+                    HStack{
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }) {
+                            
+                            Text("View All")
+                        }.foregroundColor(Color("Color1"))
+                    }
+                    
+                    Text("Continue Watching")
+                    
+                    
+                    
+                }
                 
             }
             
@@ -103,3 +123,64 @@ struct Home : View {
         .preferredColorScheme(.dark)
     }
 }
+
+struct bottomView : View {
+    
+    var body : some View{
+        
+        VStack(spacing: 15){
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                
+                HStack(spacing: 15){
+                    
+                    ForEach(datas){i in
+                        
+                        VStack(spacing: 0){
+                            
+                            Image(i.image)
+                            Button(action: {
+                                
+                                
+                            }) {
+                                
+                                Image("play").renderingMode(.original).resizable().frame(width: 25, height: 25)
+                                
+                            }.padding(.top, -12)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Here is where the sample data goes
+
+struct type : Identifiable {
+    
+    var id : Int
+    var name : String
+    var epname : String
+    var image : String
+    var percentage : CGFloat
+}
+
+var datas = [
+    type(id: 0, name: "Ghoul", epname: "Ep - S1",image: "m1",percentage : 45),
+    type(id: 1, name: "13 Reasons Why", epname: "Ep - S2",image: "m2",percentage: 90)
+]
+
+var bottom = ["b1","b2"]
+
+struct episodetype : Identifiable {
+    
+    var id : Int
+    var name : String
+    var time : String
+    var image : String
+    var desc : String
+}
+
+var episodes = [episodetype(id: 0, name: "The 'H' Word", time: "51 min", image: "d1", desc: "Matt Murdock, Jessica Jones, Luke Cage and Danny Rand investigate criminals and fight injustice, unaware their paths are about to cross."),
+episodetype(id: 1, name: "Mean RIght Hook", time: "44 min", image: "d2", desc: "As a new conspiracy takes shape, Matt finds old habits are hard to break, Jessica gets in over her head, Luke tracks a lead, and Danny meets his match.")]
